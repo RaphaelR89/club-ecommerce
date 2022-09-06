13d525b5e3d502413899febb10e9fa2b83efbb57
Raphael Rocha <rapharocha89@vidrelo.onmicrosoft.com>
2022-09-05 21:45:59 -0300

    feat:change  global style and add poppins font

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const App = () => {
+  return <h1>Hello World</h1>
 }
 
-export default App;
+export default App
