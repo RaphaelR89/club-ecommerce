cbf08406e7fb13a42ed08d6b43ee31b9bbb1b949
Raphael Rocha <rapharocha89@vidrelo.onmicrosoft.com>
2022-09-06 22:45:30 -0300

    bugfix: api  up and running

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,12 @@
 //Components
+import Categories from "../../component/categories/categories.component";
 import { Header } from "../../component/header/header.component";
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <Categories/>
     </>
   );
 };
