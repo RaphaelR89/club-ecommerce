9a28b8e5e52b18fde83b9934d02cb54a85670e00
Raphael Rocha <rapharocha89@vidrelo.onmicrosoft.com>
2022-09-12 20:05:12 -0300

    feat: userContext  updated when onAuthStateChanged

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
-import { FunctionComponent } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { onAuthStateChanged } from 'firebase/auth'
+import { FunctionComponent, useContext } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-//Component
-import HomePage from "./pages/home/HomePage";
-import LoginPage from "./pages/login/login.page";
-import SignUpPage from "./component/sign-up/sign-up.page";
+// Pages
+import HomePage from './pages/home/home.page'
+import LoginPage from './pages/login/login.page'
+import SignUpPage from './pages/sign-up/sign-up.page'
+
+// Utilities
+import { auth, db } from './config/firebase.config'
+import { UserContext } from './contexts/user.context'
+import { collection, getDocs, query, where } from 'firebase/firestore'
 
 const App: FunctionComponent = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-};
+  const { isAuthenticated, loginUser, logoutUser } = useContext(UserContext)
+  onAuthStateChanged(auth, async (user) => {
+    const isSigningOut = isAuthenticated && !user
+    if (isSigningOut) {
+      return logoutUser()
+    }
 
-export default App;
+    const isSigningIn = !isAuthenticated && user
+    if (isSigningIn) {
+      const querySnapshot = await getDocs(
+        query(collection(db, 'users'), where('id', '==', user.uid))
+      )
+
+      const userFromFirestore = querySnapshot.docs[0]?.data()
+      return loginUser(userFromFirestore as any)
+    }
+  })
+
+  console.log({ isAuthenticated })
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
